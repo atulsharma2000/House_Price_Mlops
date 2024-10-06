ed201dc3c204f9926a45f222c541d04eb4edf188
atulsharma2000 <atullsharma2000@gmail.com>
2024-10-06 10:49:30 +0530

    f

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,69 +1,90 @@
 import os
 import zipfile
-from abc import  ABC, abstractmethod
+from abc import ABC, abstractmethod
 
 import pandas as pd
 
 # defining a abstract class for my data ingester fucntion
 # this will help  me to create a class that will be able to ingest data from different sources, Also it will give security.
 # also it wll
-# I use an abstract class while designing large functional units or when I want to provide a common interface for different implementations of a component. 
+# I use an abstract class while designing large functional units or when I want to provide a common interface for different implementations of a component.
 
- # ABC: This is the base class for defining Abstract Base Classes.
-class DataIngestor(ABC):       # inherit from ABC      
-    @abstractmethod    #@abstractmethod: This decorator indicates that the abstract method ingest must be implemented by any subclass of DataIngestor.
-    def ingest(self, file_path: str) -> pd.DataFrame:   #def ingest(self, file_path: str) -> pd.DataFrame: This method signature specifies that it takes a string parameter (file_path) and returns a pandas DataFrame.
+
+# ABC: This is the base class for defining Abstract Base Classes.
+class DataIngestor(ABC):  # inherit from ABC
+    @abstractmethod  # @abstractmethod: This decorator indicates that the abstract method ingest must be implemented by any subclass of DataIngestor.
+    def ingest(
+        self, file_path: str
+    ) -> pd.DataFrame:  # def ingest(self, file_path: str) -> pd.DataFrame: This method signature specifies that it takes a string parameter (file_path) and returns a pandas DataFrame.
         "this is my abstract method to ingest data from a given file"
         pass
-    
+
+
 # implenting a concrete class for zip ingestion (get zip file and return a dataframe)
 class ZipDataIngestor(DataIngestor):
-    def ingest(self,file_path: str) -> pd.DataFrame:
+    def ingest(self, file_path: str) -> pd.DataFrame:
         """Extracts a .zip file and returns the content as a pandas DataFrame."""
         # Ensuring the file is a .zip
         if not file_path.endswith(".zip"):
             raise ValueError("The provided file is not a .zip file.")
-        
+
         # Extract the zip file and place it in extracted_data
-        with zipfile.ZipFile(file_path,'r') as zip_file:
-            zip_file.extractall("extracted_data")  #It extracts all contents of the ZIP file into a directory named extracted_data. If this directory does not exist, it will be created automatically.
-        
+        with zipfile.ZipFile(file_path, "r") as zip_file:
+            zip_file.extractall(
+                "extracted_data"
+            )  # It extracts all contents of the ZIP file into a directory named extracted_data. If this directory does not exist, it will be created automatically.
+
         # find the extracted csv file (assuming  it is the only one csv file inside zip)
         extracted_files = os.listdir("extracted_data")
         csv_files = [f for f in extracted_files if f.endswith(".csv")]
-        
+
         # checking if there are any csv files or not in csv_files
-        if len(csv_files)==0:
-            raise  FileNotFoundError("No CSV file found in the extracted data.")
+        if len(csv_files) == 0:
+            raise FileNotFoundError("No CSV file found in the extracted data.")
         if len(csv_files) > 1:
-            raise ValueError("More than one CSV file found in the extracted data. Please ensure there is only one or specify")
+            raise ValueError(
+                "More than one CSV file found in the extracted data. Please ensure there is only one or specify"
+            )
 
         # reading the csv into a DataFrame
         # Assuming there is exactly one CSV file, this code constructs its full path and reads it into a pandas DataFrame using pd.read_csv.
-        csv_file_path = os.path.join("extracted_data",csv_files[0])
+        csv_file_path = os.path.join("extracted_data", csv_files[0])
         df = pd.read_csv(csv_file_path)
-        
-        # returning the extracted data frame 
+        # The primary purpose of os.path.join() is to create a valid file path by joining multiple directory or file names together using the appropriate separator for the operating system (e.g., / for Unix-like systems and \ for Windows).
+
+        # returning the extracted data frame
         return df
 
 
 #  A factory class is a design pattern used to create objects without specifying the exact class of object that will be created.
 class DataIngestFactory:
-    @staticmethod    #@staticmethod: This decorator indicates that the method get_data_ingestor does not require access to any instance-specific data (i.e., it does not need to access self). It can be called on the class itself rather than on an instance of the class.
-    def get_data_ingetor(file_extension: str) -> DataIngestor:
+    @staticmethod  # @staticmethod: This decorator indicates that the method get_data_ingestor does not require access to any instance-specific data (i.e., it does not need to access self). It can be called on the class itself rather than on an instance of the class.
+    def get_data_ingestor(file_extension: str) -> DataIngestor:
         """Returns a DataIngestor instance based on the file extension."""
         if file_extension == ".zip":
             return ZipDataIngestor()
         else:
-            raise ValueError(f"Unsupported file extension. Only .zip files are supported at this time not '{file_extension}'.")
-    
-    
-if __name__=="__main__":
-    pass
-    
-    
+            raise ValueError(
+                f"Unsupported file extension. Only .zip files are supported at this time not '{file_extension}'."
+            )
 
 
+if __name__ == "__main__":
+    # specify the file path
+    # Prefixing the string with r tells Python to treat backslashes as literal characters and not as escape characters.
+    file_path = r"C:\Users\Admin\Desktop\Contains_proj\House_Price_Mlops\data\archive.zip"
+
+    # determining the file extension
+    file_extension = os.path.splitext(file_path)[1]
+
+    # get the proper appropriate DataIngestor
+    data_ingestor = DataIngestFactory.get_data_ingestor(file_extension)
+
+    # Ingest the data and load it into a dataframe
+    df = data_ingestor.ingest(file_path)
+
+    # now df contains the DataFrame from the extracted csv
+    print(df.head())
 
 
 # ---------------------------ignore-------------------------
